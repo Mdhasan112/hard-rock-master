ee1726a3eeb3edaed4bbb05bff012faba05232b1
hasan <mdhasanhk.98@gmail.com>
2022-08-08 16:48:01 -0400

    hard rock

--- app.js
+++ app.js
@@ -3,5 +3,41 @@ const searchSong = () => {
     const url =  `https://api.lyrics.ovh/suggest/${searchText}`
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displaySong(data.data))
+}
+
+const displaySong = (song) => {
+    const songContainer = document.getElementById('song-container');
+    songContainer.innerHTML = '';
+
+    song.forEach(song => {
+        const songDiv = document.createElement('div');
+        songDiv.className = "single-result row align-items-center my-3 p-3";
+        songDiv.innerHTML = `
+        <div class="col-md-9">
+        <h3 class="lyrics-name">${song.title}</h3>
+        <p class="author lead">Album by <span>${song.artist.name}</span></p>
+        <audio controls>
+        <source src="${song.preview}" type="audio/ogg">
+        </audio>
+        </div>
+        <div class="col-md-3 text-md-right text-center">
+        <button onclick = "getLyrics('${song.artist.name}', '${song.title}',)" class="btn btn-success">Get Lyrics</button>
+        </div>
+        `;
+
+        songContainer.appendChild(songDiv)
+    }) 
+}
+
+const getLyrics = (artist, title) => {
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayLyrics(data.lyrics))
+}
+
+const displayLyrics = lyrics => {
+    const songLyrics = document.getElementById('song-lyrics');
+    songLyrics.innerText = lyrics;
 }
